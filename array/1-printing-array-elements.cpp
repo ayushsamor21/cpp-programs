cf1ac4241aa2cc28e2196e334303a6c67839b6a8
ayush samor <ayushsamor21@gmail.com>
2025-07-15 19:55:53 +0530

    array-programs

---
+++ array/1-printing-array-elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector> 
+using namespace std;
+
+int main(){
+    int sizeOfArray;
+    cout << "Please enter the size of array: ";
+    cin >> sizeOfArray;
+
+    vector<int> array(sizeOfArray, 0);
+
+    for(int i = 0; i < sizeOfArray; i++){
+        cout << "The value at index["<< i <<"]: ";
+        cin >> array[i];
+    }
+
+    cout << "The array elements :\n";
+    for(int i = 0; i < sizeOfArray; i++){
+        cout << "Index["<< i <<"]: "<< array[i] << endl;
+    }
+    return 0;
+}
