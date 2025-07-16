ec63afaec3cd1cdd00fe98a1721b330c7b69323e
ayush samor <ayushsamor21@gmail.com>
2025-07-16 17:47:10 +0530

    reverse element

---
+++ array/5-reverse-element.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector> 
+using namespace std;
+
+int main(){
+    int sizeOfArray;
+    cout << "Please enter the size of array: ";
+    cin >> sizeOfArray;
+
+    if (sizeOfArray <= 0){
+        cout << "Invalid array size!!\n";
+        return 1;
+    }
+
+    vector<int> array(sizeOfArray, 0);
+
+    for(int i = 0; i < sizeOfArray; i++){
+        cout << "The value at index["<< i <<"]: ";
+        cin >> array[i];
+    }
+
+    vector<int> revArray(sizeOfArray, 0);
+
+    for(int i = 0; i < sizeOfArray; i++){
+        revArray[i] = array[sizeOfArray - 1 - i];
+    }
+
+    cout <<"The reversed array: ";
+     for(int i = 0; i < sizeOfArray; i++){
+        cout << "The value at Index["<< i <<"]:"<< revArray[i] << endl;
+    }
+    return 0;
+}
