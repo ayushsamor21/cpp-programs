ecac53aff2d48e5fa38aba9929b0da27cd83878e
ayush samor <ayushsamor21@gmail.com>
2025-07-16 15:50:42 +0530

    search value in array

---
+++ array/2-search-array-element.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int sizeOfArray, searchValue;
+    cout << "Please enter the size of array: ";
+    cin >> sizeOfArray;
+    vector<int> array(sizeOfArray, 0);
+
+    for(int i = 0; i < sizeOfArray; i++){
+        cout <<"Please enter the element at index["<< i <<"]:";
+        cin >> array[i];
+    }
+
+    cout << "Please enter the element to search: ";
+    cin >> searchValue;
+
+    for(int i = 0; i < sizeOfArray; i++){
+        if (array[i] == searchValue){
+            cout << "The element "<< searchValue <<" is present at index["<< i <<"]";
+        }
+    }
+    return 0;
+}
