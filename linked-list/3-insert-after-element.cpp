42ba6701a76495d08c070a2db4ed28fca8e61b9a
ayush samor <ayushsamor21@gmail.com>
2025-10-11 14:23:47 +0530

    insert after element

---
+++ linked-list/3-insert-after-element.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+ 
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList{
+private:
+    Node* head;
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    ~LinkedList() {
+        freeList();
+    }
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data;
+            if (temp->next != nullptr)
+                cout << "->";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void insertAfterElement(int targetValue, int newValue) {
+        Node* temp = head;
+
+        // Find the target node
+        while (temp != nullptr && temp->data != targetValue) {
+            temp = temp->next;
+        }
+
+        if (temp == nullptr) {
+            cout << "Node not found!!";  // Target not found 
+            return;
+        }
+
+        // Insert after target
+        Node* newNode = new Node(newValue);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void freeList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    int sizeOfList, element, targetNode, insertValue;
+    cout << "Please enter the size of linked list: ";
+    cin >> sizeOfList;
+
+    for (int i = 0; i < sizeOfList; i++) {
+        cout << "Please enter Element : ";
+        cin >> element;
+        list.append(element);
+    }
+
+    cout << "---Original Linked List---\n";
+    list.print();
+
+    cout << "Please enter the node to insert after: ";
+    cin >> targetNode;
+
+    cout << "Please enter the  new node value: ";
+    cin >> insertValue;
+
+    list.insertAfterElement(targetNode, insertValue);
+    cout << "---New Linked List---\n";
+    list.print();
+
+    return 0;
+}
