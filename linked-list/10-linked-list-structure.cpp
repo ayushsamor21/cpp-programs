d08cb879a30014bc252e23a21ba109730494b194
ayushsamor21 <ayushsamor21@gmail.com>
2026-01-01 09:52:10 +0530

    Linked List structure

---
+++ linked-list/10-linked-list-structure.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node (int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void append (int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void printList() {
+        Node* temp = head;
+        while(temp != nullptr) {
+            cout << temp->data;
+            if (temp->next != nullptr) {
+                cout << "->";
+            }
+            temp = temp->next;
+        }
+    }
+
+    void freeList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    ~LinkedList() {
+        freeList();
+    }
+};
+
+int main() {
+    int numberOfElement, element;
+    cout << "Please enter the number of elements: ";
+    cin >> numberOfElement;
+
+    LinkedList list;
+
+    for (int i = 0; i < numberOfElement; i++) {
+        cout << "Please enter the element: ";
+        cin >> element;
+        list.append(element);
+    }
+
+    list.printList();
+    return 0;
+}
