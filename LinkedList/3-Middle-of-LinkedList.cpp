e2e84a0032c9e5fd4aef48875d4572bc832c0c1e
ayush samor <ayushsamor21@gmail.com>
2026-01-24 11:13:34 +0530

    OP: Middle of Linkedlist

---
+++ LinkedList/3-Middle-of-LinkedList.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+public:
+    LinkedList () {
+        head = nullptr;
+    }
+
+    void append (int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        return;
+    }
+
+    void printLinkedList () {
+        Node* temp = head;
+        while ( temp != nullptr) {
+            cout << temp->data;
+            if ( temp->next != nullptr) {
+                cout << "->";
+            }
+            temp = temp->next;
+        }
+    }
+
+    void middleLinkedList() {
+        Node* slow = head;
+        Node* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        cout << "\nThe middle element: " << slow->data << endl;
+    }
+
+    void freeList () {
+        Node* temp;
+        while(head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    ~LinkedList() {
+        freeList();
+    }
+};
+
+int main() {
+    LinkedList list;
+    int numOfElements, num;
+
+    cout << "Please enter the number of elements: ";
+    cin >> numOfElements;
+
+    for (int i = 0; i < numOfElements; i++) {
+        cout << "Please enter the element: ";
+        cin >> num;
+        list.append(num);
+    }
+
+    cout << "Linked List: ";
+    list.printLinkedList();
+
+    list.middleLinkedList();
+
+    return 0;
+}
