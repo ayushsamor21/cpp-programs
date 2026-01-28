4adcd1e440ab6c0246775e82be0c6bb2a8b339ea
ayush samor <ayushsamor21@gmail.com>
2026-01-28 20:58:25 +0530

    Bubble Sort

---
+++ sorting/3-bubble-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;   
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break; 
+    }
+}
+
+
+// Print Array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i ++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+    cout << "Sorted Array: ";
+    printArray(arr, n);
+    return 0;
+}
