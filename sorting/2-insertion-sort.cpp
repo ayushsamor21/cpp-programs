90c8fe0baab00949b1a2b6d82e0c82f249a5c090
ayush samor <ayushsamor21@gmail.com>
2026-01-28 18:53:12 +0530

    Inserstion Sort

--- sorting/2-insertion-sort.cpp
+++ sorting/2-insertion-sort.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// Complexity : O(n^2)
+// best case : O(n)
+
 // Insertion Sort
 void insertionSort(int arr[], int n) {
     int num, j;
@@ -28,6 +31,23 @@ int main() {
     int n = sizeof(arr) / sizeof(arr[0]);
 
     insertionSort(arr, n);
+    cout << "Sorted Array: ";
     printArray(arr, n);
     return 0;
 }
+
+// Using two for loops 
+// void insertionSort(int arr[], int n) {
+//     int num, j;
+//     for (int i = 1; i < n; i++) {
+//         num = arr[i];
+//         for (j = i - 1; j >= 0; j --) {
+//             if (arr[j] > num) {
+//                 arr[j + 1] = arr[j];
+//             }else {
+//                 break;
+//             }
+//         }
+//         arr[j + 1] = num;
+//     }
+// }
